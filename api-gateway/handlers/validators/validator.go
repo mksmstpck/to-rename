20a2275bebe63a59a44f99d2862a697592d29042
max <mksmstpck@gmail.com>
2023-02-09 15:07:27 +0200

    feat(api-gateway.handlers): add custom JSON validator

---
+++ api-gateway/handlers/validators/validator.go
@@ -0,0 +1,25 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/go-playground/validator"
+	"github.com/labstack/echo/v4"
+)
+
+type CustomValidator struct {
+	validator *validator.Validate
+}
+
+func NewCustomeValidator() *CustomValidator {
+	return &CustomValidator{
+		validator: validator.New(),
+	}
+}
+
+func (cv *CustomValidator) Validate(i interface{}) error {
+	if err := cv.validator.Struct(i); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+	return nil
+}

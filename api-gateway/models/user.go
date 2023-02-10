403b0c6a053c700bfcc31cda6d839ce475811797
max <mksmstpck@gmail.com>
2023-02-10 19:15:26 +0200

    feat(api-gateway): add validator options

--- api-gateway/models/user.go
+++ api-gateway/models/user.go
@@ -2,21 +2,21 @@ package models
 
 type Permission struct {
 	ID        string   `json:"id"`
-	Name      string   `json:"name"`
-	AllowedTo []string `json:"allowedTo"`
+	Name      string   `json:"name" validate:"required,name"`
+	AllowedTo []string `json:"allowedTo" validate:"required,allowedTo"`
 }
 
 type Role struct {
 	ID          string       `json:"id"`
-	Name        string       `json:"name"`
-	Permissions []Permission `json:"permissions"`
+	Name        string       `json:"name" validate:"required,name"`
+	Permissions []Permission `json:"permissions" validate:"required,permissions"`
 }
 
 type User struct {
 	ID       string `json:"id"`
-	Username string `json:"username"`
-	FullName string `json:"fullName"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
-	Role     Role   `json:"role"`
+	Username string `json:"username" validate:"required,name"`
+	FullName string `json:"fullName" validate:"required,fullname"`
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required,password"`
+	Role     Role   `json:"role" validate:"required,role"`
 }

02cd64ac164075532ae2bb739f1ebbf9af25bfdc
max <mksmstpck@gmail.com>
2023-02-10 22:28:48 +0200

    feat(api-gateway.events) add updateuser

--- api-gateway/events/user.go
+++ api-gateway/events/user.go
@@ -9,7 +9,7 @@ import (
 
 func (p *Pub) GetUser(id []byte) (models.User, error) {
 	var user models.User
-	m, err := p.conn.Request("users", id, 10*time.Millisecond)
+	m, err := p.conn.Request("users-get", id, 10*time.Millisecond)
 	if err != nil {
 		panic(err)
 	}
@@ -23,7 +23,27 @@ func (p *Pub) WriteUser(user models.User) error {
 	if err != nil {
 		return err
 	}
-	m, err := p.conn.Request("users", userBytes, 10*time.Millisecond)
+	m, err := p.conn.Request("users-post", userBytes, 10*time.Millisecond)
+	if err != nil {
+		return err
+	}
+	sonic.Unmarshal(m.Data, &res)
+	if res.Status == "ok" {
+		return nil
+	}
+	if res.Status != "ok" {
+		return err
+	}
+	return nil
+}
+
+func (p *Pub) UpdateUser(user models.User) error {
+	var res models.Response
+	userBytes, err := sonic.Marshal(user)
+	if err != nil {
+		return err
+	}
+	m, err := p.conn.Request("users-put", userBytes, 10*time.Millisecond)
 	if err != nil {
 		return err
 	}

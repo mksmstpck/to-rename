9f6a0f2675f6766b18a74ed53664040816e0adea
max <mksmstpck@gmail.com>
2023-03-07 12:22:55 +0200

    feat(user-service.services): add password hashing

---
+++ user-service/services/passwordHash.go
@@ -0,0 +1,11 @@
+package services
+
+import "golang.org/x/crypto/bcrypt"
+
+func PasswordHash(password string) string {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		panic(err)
+	}
+	return string(hash)
+}

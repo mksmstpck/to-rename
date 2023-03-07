b395dfd76f9464a661e7fe469fe91f497f9f3ace
max <mksmstpck@gmail.com>
2023-03-07 22:40:04 +0200

    feat(user-service.config): add logic to config

--- user-service/config/config.go
+++ user-service/config/config.go
@@ -1,1 +1,26 @@
 package config
+
+import "github.com/spf13/viper"
+
+type Config struct {
+	PSQLhost string `mapstructure:"PSQL_HOST"`
+	PSQLport string `mapstructure:"PSQL_PORT"`
+	PSQLuser string `mapstructure:"PSQL_USER"`
+	PSQLpass string `mapstructure:"PSQL_PASS"`
+	PSQLdb   string `mapstructure:"PSQL_DB"`
+	NatsUrl  string `mapstructure:"NATS_URL"`
+}
+
+func NewConfig() (c Config, err error) {
+	viper.AddConfigPath(".")
+	viper.SetConfigName("dev")
+	viper.SetConfigType("env")
+
+	if err = viper.ReadInConfig(); err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&c)
+
+	return
+}
